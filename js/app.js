a105eb138896df53dfd5e91322448a2afe9dbc42
Jerry Chen <jychen216@gmail.com>
2016-08-20 15:41:26 -0700

    Inject $location

--- js/app.js
+++ js/app.js
@@ -25,51 +25,56 @@ app.config(function($stateProvider, $urlRouterProvider) {
         });
 });
 
-app.controller("myCtrl", function ($scope) {
+app.controller("myCtrl", function ($scope, $location) {
     $scope.beerSearch = "";
     $scope.beers = [{
-        "name": "Deschutes Mirror Pond Pale Ale",
+        "name": "mirrorpond",
+        "keywords": "Deschutes Mirror Pond Pale Ale",
         "brewery": "Deschutes",
         "image": "../images/deschutes_mirrorpond.png",
         "description": "BEST PALE ALE"
     },
     {
-        "name": "Deschutes Chainbreaker White IPA",
+        "name": "chainbreaker",
+        "keywords": "Deschutes Chainbreaker White IPA",
         "brewery": "Deschutes",
         "image": "../images/deschutes_chainbreaker.png",
         "description": "BEST WHITE IPA"
     },
     {
-        "name": "Pabst Blue Ribbon",
+        "keywords": "Pabst Blue Ribbon",
         "brewery": "Pabst",
         "image": "../images/pabst_pbr.jpg",
         "description": "BEST CHEAP BEER"
     },
     {
-        "name": "Pabst Rainier Beer PBR",
+        "keywords": "Pabst Rainier Beer PBR",
         "brewery": "Pabst",
         "image": "../images/pabst_rainier.jpg",
         "description": "BEST CHEAP BEER IN THE PNW"
     },
     {
-        "name": "Rogue Dead Guy Ale",
+        "keywords": "Rogue Dead Guy Ale",
         "brewery": "Rogue",
         "image": "../images/rogue_deadguy.jpg",
         "description": "BEST BOCK ON THE BLOCK"
     },
     {
-        "name": "Rogue Hazelnut Brown Nectar",
+        "keywords": "Rogue Hazelnut Brown Nectar",
         "brewery": "Rogue",
         "image": "../images/rogue_hazelnut.jpg",
         "description": "BEST BROWN ALE"
     },
     {
-        "name": "Sly Fox Helles Golden Lager",
+        "keywords": "Sly Fox Helles Golden Lager",
         "brewery": "Sly Fox",
         "image": "../images/slyfox_helles.png",
         "description": "BEST LAGER"
     }];
     $scope.descriptions = {
+        "main": {
+            "title": "Click on a brewery to see a list of its beers\nor type in the name of a beer in the search bar."
+        },
         "deschutes": {
             "title": "Deschutes Brewery",
             "subtitle": "Bend, Oregon",
@@ -90,7 +95,7 @@ app.controller("myCtrl", function ($scope) {
             "subtitle": "Phoenixville, Pennsylvania",
             "description": "Sly Fox was founded in 1995 by the Giannopoulos family, who hunted foxes in the Chester County area."
         },
-        "Deschutes Chainbreaker White IPA": {
+        "chainbreaker": {
             "title": "Chainbreaker",
             "subtitle": "White IPA",
             "description": "Chainbreaker White IPA is the best white IPA."
