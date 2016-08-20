58e82f1c10fe4ed8caa874ab2c96f58f845a321c
Jerry Chen <jychen216@gmail.com>
2016-08-20 15:10:09 -0700

    Add brewery descriptions

--- js/app.js
+++ js/app.js
@@ -7,10 +7,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
         url: "/main",
         templateUrl: "main.html"
         })
-        .state('search', {
-            url: "/search",
-            templateUrl: "search.html"
-        })
         .state('deschutes', {
             url: "/deschutes",
             templateUrl: "deschutes.html"
@@ -73,6 +69,33 @@ app.controller("myCtrl", function ($scope) {
         "image": "../images/slyfox_helles.png",
         "description": "BEST LAGER"
     }];
+    $scope.descriptions = {
+        "deschutes": {
+            "title": "Deschutes Brewery",
+            "subtitle": "Bend, Oregon",
+            "description": "Deschutes Brewery was founded in 1988 as a brew pub. In 2008, it opened a second brew pub in Portland's Pearl District. Deschutes is one of the largest craft breweries in the U.S., producing 250,000 barrels a year."
+        },
+        "pabst": {
+            "title": "Pabst Brewing Co.",
+            "subtitle": "Los Angeles, California",
+            "description": "Pabst was founded in 1844 by Jacob Best. Originally founded in Milwaukee, Wisconsin, Pabst has grown into a holding company with over two dozen portfolio brands."
+        },
+        "rogue": {
+            "title": "Rogue Ales",
+            "subtitle": "Newport, Oregon",
+            "description": "Rogue Ales was founded in 1988 in Ashland, Oregon. Operating brew pubs in Oregon, Washington, and California, Rogue has expanded into distilling as well."
+        },
+        "slyfox": {
+            "title": "Sly Fox Brewery",
+            "subtitle": "Phoenixville, Pennsylvania",
+            "description": "Sly Fox was founded in 1995 by the Giannopoulos family, who hunted foxes in the Chester County area."
+        },
+        "Deschutes Chainbreaker White IPA": {
+            "title": "Chainbreaker",
+            "subtitle": "White IPA",
+            "description": "Chainbreaker White IPA is the best white IPA."
+        }
+    };
     $scope.displaySearch = function(text) {
         if (text.length) {
             //display search view
@@ -81,5 +104,5 @@ app.controller("myCtrl", function ($scope) {
         //display default
         return $location.path("/main");
     };
-    $scope.description = "";
+    $scope.lastClick = "";
 });
